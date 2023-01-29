c64a0b55baf9840589e578b117f56b681c565d1f
Furkan <89701935+KodBilen42@users.noreply.github.com>
2023-01-29 17:27:26 +0300

    dqn

--- Deep Learning.py
+++ Deep Learning.py
@@ -47,41 +47,100 @@ def generate_model(input_shape, action_space):
     model.summary()
     return model
 
-vehicle = None
 def env_reset(carla_environment):
-    global vehicle
     carla_environment.clear_objects()
     vehicle = Vehicle(carla_environment, "model3", False, 0)
+    return vehicle
+
+class TrainingEnvironment:
+
+    def __init__(self, delta_seconds=1/15.0, no_rendering_mode=False, port=2000):
+        self.delta_seconds = delta_seconds
+        self.no_rendering_mode = no_rendering_mode
+        self.port = port
+
+        self.simulation_duration = 30  # runs are 300 seconds long (in-simulation time)
+
+        self.carla_env = None
+        self.vehicle = None
+        self.reset()
+
+
+    def step(self, action): # actions are only an index, fix it
+        throttle = 0
+        steer = 0
+        brake = 0
+        if action == 0:
+            throttle = 0
+            brake = 0
+            steer = 0
+        elif action == 1:
+            throttle = 0.6
+            brake = 0
+            steer = 0
+        elif action == 2:
+            throttle = 0.4
+            steer = 1
+            brake = 0
+        elif action == 3:
+            throttle = 0.4
+            steer = -1
+            brake = 0
+        elif action == 4:
+            throttle = 0
+            steer = 0
+            brake = 1
+
+
+        self.vehicle.apply_control(throttle=throttle, steer=steer, brake=brake)
+        self.carla_env.step(True)
+        next_state = self._get_state()
+
+        reward = self._get_reward()
+        print(reward)
+
+        done = self.carla_env.simulated_time >= self.simulation_duration
+        return next_state, reward, done, None
+
+    def reset(self):
+        self.carla_env = CarlaEnvironment(delta_seconds=self.delta_seconds, no_rendering_mode=self.no_rendering_mode, port=self.port)
+        self.carla_env.change_map('town03')
+        self.vehicle = Vehicle(self.carla_env, spawn_point=0)
+        return self._get_state()
+
+    def _get_state(self):
+        position = self.vehicle.get_position()
+        return [position.x, position.y, position.z]
+
+    def _get_reward(self):
+        current_position = self.vehicle.get_position()
+        return current_position.length()
 
 class DQNAgent:
 
     def __init__(self):
-        # by default, CartPole-v1 has max episode steps = 500
-        self.env = CarlaEnvironment(delta_seconds=1/15, no_rendering_mode=False, port=4221)
-        self.env.change_map("town03")
-        self.vehicle = Vehicle(self.env, spawn_point=0)
+        self.env = TrainingEnvironment(delta_seconds=1/15, no_rendering_mode=False, port=4565)
 
-        self.state_size = 2
-        self.action_size = 4
+        self.state_size = 3
+        self.action_size = 5
         self.EPISODES = 1000
         self.memory = deque(maxlen=1024 * 4)
 
-        self.gamma = 0.95  # discount rate
+        self.gamma = 0.95  # discount rate, how important long term rewards are compared the short term rewards, 1 means identical, 0 means have no importance
         self.epsilon = 1.0  # exploration rate
         self.epsilon_min = 0.03
-        self.epsilon_decay = 0.999
+        self.epsilon_decay = 0.92
         self.batch_size = 1024
         self.epoch_count = 4
         self.train_start = 1024
-        self.visualize = True
-        self.image_width = 64
-        self.image_height = 64
 
         # create main model
         self.model = generate_model(input_shape=(self.state_size,), action_space=self.action_size)
 
     def remember(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done))
+
+    def decay_epsilon(self):
         if len(self.memory) >= self.train_start:
             if self.epsilon >= self.epsilon_min:
                 self.epsilon *= self.epsilon_decay
@@ -91,15 +150,6 @@ class DQNAgent:
             return random.randrange(self.action_size)
         else:
             return np.argmax(self.model.predict(state, verbose=0))
-
-    def step(self):
-        state = self.vehicle.get_position()
-        self.env.step(True)
-        control = self.vehicle.actor.get_control()
-        next_state = self.vehicle.get_position()
-        self.vehicle.update_total_distance_travelled()
-        reward = self.vehicle.total_distance_travelled # TODO: define reward
-        return [state.x, state.y, state.z], [control.throttle, control.steer, control.brake], reward, [next_state.x, next_state.y, next_state.z]
 
     def replay(self):
         if len(self.memory) < self.train_start:
@@ -134,7 +184,8 @@ class DQNAgent:
                 target[i][action[i]] = reward[i] + self.gamma * (np.amax(target_next[i]))
 
         # Train the Neural Network with batches
-        self.model.fit(state, target, batch_size=self.batch_size, epochs=self.epoch_count, verbose=0)
+        self.model.fit(state, target, batch_size=self.batch_size, epochs=self.epoch_count, verbose=1)
+        self.decay_epsilon()
 
     def load(self, name):
         self.model = load_model(name)
@@ -149,26 +200,25 @@ class DQNAgent:
             done = False
             i = 0
             while not done:
-                if self.visualize:
-                    self.env.render()
                 action = self.act(state)
                 next_state, reward, done, _ = self.env.step(action)
                 next_state = np.reshape(next_state, [1, self.state_size])
-                if not done or i == self.env._max_episode_steps - 1:
+                if not done:
                     reward = reward
                 else:
-                    reward = -100
+                    reward = -100   # I'm not sure about this
+
                 self.remember(state, action, reward, next_state, done)
                 state = next_state
                 i += 1
                 if done:
                     print("episode: {}/{}, score: {}, e: {:.2}".format(e, self.EPISODES, i, self.epsilon))
                     print("Saving trained model as dqn.h5")
-                    self.save("dqn.h5")
+                    self.save("simple dqn.h5")
                     #return
             self.replay()
 
 
 agent = DQNAgent()
 while True:
-    agent.step()
+    agent.run()
